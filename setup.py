989eb0545815a1167ed119ce1ce889badf18eee7
Will Barton <william.barton@cfpb.gov>
2023-03-22 13:28:47 -0400

    Switch to pyproject.toml for packaging

--- setup.py
+++ setup.py
@@ -1,41 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
-install_requires = [
-    "Markdown>=3.2,<3.5",
-]
-
-
-testing_extras = [
-    "coverage>=3.7.0",
-]
-
-
-setup(
-    name="regdown",
-    url="https://github.com/cfpb/regdown",
-    author="CFPB",
-    author_email="tech@cfpb.gov",
-    description="Markdown extension for interactive regulations",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="CC0",
-    version="1.0.5",
-    include_package_data=True,
-    packages=find_packages(),
-    test_suite="regdown.tests",
-    install_requires=install_requires,
-    extras_require={
-        "testing": testing_extras,
-    },
-    classifiers=[
-        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "License :: Public Domain",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-    ],
-)
+setup()
